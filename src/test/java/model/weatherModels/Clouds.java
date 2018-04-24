08a14a7ca79a790ecbaa850db66677248ae21b46
Flexo <aizek48@gmail.com>
2018-04-24 21:40:50 +0300

    Eeeee

---
+++ src/test/java/model/weatherModels/Clouds.java
@@ -0,0 +1,13 @@
+package model.weatherModels;
+
+public class Clouds {
+
+    private Integer all;
+
+    public Integer getAll() { return  all; }
+
+    public void setAll(Integer all) {
+        this.all = all;
+    }
+}
+
---
+++ src/test/java/model/weatherModels/Sys.java
@@ -0,0 +1,66 @@
+package model.weatherModels;
+
+import java.math.BigDecimal;
+
+public class Sys {
+
+    private Integer type;
+    private Integer id;
+    private BigDecimal message;
+    private String country;
+    private Integer sunrise;
+    private Integer sunset;
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public BigDecimal getMessage() {
+        return message;
+    }
+
+    public void setMessage(BigDecimal message) {
+        this.message = message;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Integer getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(Integer sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public Integer getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(Integer sunset) {
+        this.sunset = sunset;
+    }
+
+
+}
+
+
+
